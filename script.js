10a9f5fea8133c118cc30a46d30d37bacb4c982a
Rupesh <vrupeshmoorthi@gmail.com>
2024-10-12 07:46:36 +0530

    add

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
 function add(){
             
 }
+function btn1(){
+const r=Math.random();
+            let resultss={
+                Heads:0,
+                Tails:0
+            };
+ let c='';
+    if(0<=r && r<0.5){
+        c='Heads'
+    }
+    else if(0.5<=r && r<=1){
+        c='Tails'
+    }
+    let result=''
+    if(c==='Heads'){
+        result='You Won The Toss'
+        resultss.Heads++;
+    }
+    else if(c==='Tails'){
+        result='You Lost The Toss'
+        resultss.Tails++;
+    }
+    alert(`You choos Heads, ${result}
+    Won Toss ${resultss.Heads} , Losses Toss ${resultss.Tails}`)
+    
+}
+function btn2(){
+    const r=Math.random();
+    let score={
+        Heads:0,
+        Tails:0
+        };
+    let c='';
+    if(0<=r && r<0.5){
+        c='Heads'
+    }
+    else if(0.5<=r && r<=1){
+        c='Tails'
+    }
+    let result=''
+    if(c==='Heads'){
+        result='You Lost The Toss'
+        score.Heads++;
+    }
+    else if(c==='Tails'){
+        result='You Won The Toss'
+        score.Tails++;
+    }
+    alert(`You choos Tails, ${result}
+Won Toss ${score.Tails} , Losses Toss ${score.Heads}`)
+}
